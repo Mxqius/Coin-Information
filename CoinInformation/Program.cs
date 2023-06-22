b9e5c3333399face704257cdd2dd390c03f4a1f6
Ashkan <maryamsoltanebrahimi@gmail.com>
2023-06-22 15:10:56 +0330

    Add project files.

---
+++ CoinInformation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace CoinInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            typingPrint("Hello, Welcome to the CoinInformation!", 50);
+            Console.WriteLine("\nLoading Information...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            CoinInfo coinInfo = new CoinInfo();
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+            while (true)
+            {
+                List<Coin> coins = coinInfo.GetAllCoins().Result;
+
+                foreach (Coin coin in coins)
+                {
+                    Console.WriteLine($"Name: {coin.Name}");
+                    Console.WriteLine($"Symbol: {coin.Symbol}");
+                    Console.WriteLine($"Price: {coin.CurrentPrice.ToString("C", cultureInfo)}");
+                    Console.WriteLine($"Market Cap: {coin.MarketCap}");
+                    Console.WriteLine();
+                }
+                Thread.Sleep(300_000);
+                Console.Clear();
+            }
+            //Console.ReadKey();
+        }
+
+        static void typingPrint(string message, int delayMs)
+        {
+            foreach (char chr in message)
+            {
+                Console.Write(chr);
+                Thread.Sleep(delayMs);
+            }
+        }
+    }
+}
---
+++ CoinInformation/Services/CoinInfo.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class CoinInfo
+{
+    private readonly HttpClient _httpClient;
+
+    public CoinInfo()
+    {
+        HttpClientHandler handler = new HttpClientHandler
+        {
+            Proxy = new WebProxy("46.4.96.137", 1080),
+            UseProxy = true
+        };
+
+        _httpClient = new HttpClient();
+
+        _httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
+    }
+
+    public async Task<List<Coin>> GetAllCoins()
+    {
+        List<Coin> coins = new List<Coin>();
+
+        try
+        {
+            string apiUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1"; // CoinGecko API endpoint
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+            coins = JsonConvert.DeserializeObject<List<Coin>>(responseContent);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error retrieving coin information: {ex.Message}");
+        }
+
+        return coins;
+    }
+}
+
+public class Coin
+{
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("symbol")]
+    public string Symbol { get; set; }
+
+    [JsonProperty("current_price")]
+    public decimal CurrentPrice { get; set; }
+
+    [JsonProperty("market_cap")]
+    public decimal MarketCap { get; set; }
+    // Add more properties as needed
+}
+
